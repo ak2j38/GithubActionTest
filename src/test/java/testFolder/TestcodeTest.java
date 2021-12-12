f0977a70713596ce5adc545acdad246b7013a43e
WooJin Park <ak2j38@gmail.com>
2021-12-12 19:42:25 +0900

    Build : github action Test(fail5)

--- src/test/java/testFolder/TestcodeTest.java
+++ src/test/java/testFolder/TestcodeTest.java
@@ -14,6 +14,12 @@ public class TestcodeTest {
     }
 
     @Test
+    @DisplayName("깃헙 액션을 위한 성공 테스트2")
+    void suc_Test2() {
+        assertThat(true).isEqualTo(true);
+    }
+
+    @Test
     @DisplayName("깃헙 액션을 위한 실패 테스트")
     void fail_Test() {
         assertThat(true).isEqualTo(false);
