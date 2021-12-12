a874871dd27d8c6a9f391f96ebd4dd6810f8afd1
WooJin Park <ak2j38@gmail.com>
2021-12-12 20:55:27 +0900

    Build : github action Test(formatting)

---
+++ src/main/java/needFormatting.java
@@ -0,0 +1,7 @@
+public class needFormatting {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++){
+            System.out.println("need formatting "+i);
+        }
+    }
+}
