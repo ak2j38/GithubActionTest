abfe7603797713f5f820d1f273efe02a9655bcf0
WooJin Park <ak2j38@gmail.com>
2021-12-12 22:22:55 +0900

    Build : github action Test(success) with new branch2

--- src/test/java/testFolder/testcodeTest2.java
+++ src/test/java/testFolder/testcodeTest2.java
@@ -1,4 +1,14 @@
 package testFolder;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class testcodeTest2 {
+    @Test
+    @DisplayName("깃헙 액션을 위한 성공 테스트3")
+    void suc_Test() {
+        assertThat(true).isEqualTo(true);
+    }
 }
