8f4222b23698caf1993ee97ea78a55598058a673
WooJin Park <ak2j38@gmail.com>
2021-12-12 19:20:31 +0900

    Build : github action Test(fail4)

---
+++ src/test/java/testFolder/TestcodeTest.java
@@ -0,0 +1,22 @@
+package testFolder;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestcodeTest {
+
+    @Test
+    @DisplayName("깃헙 액션을 위한 성공 테스트")
+    void suc_Test() {
+        assertThat(true).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("깃헙 액션을 위한 실패 테스트")
+    void fail_Test() {
+        assertThat(true).isEqualTo(false);
+    }
+
+}
